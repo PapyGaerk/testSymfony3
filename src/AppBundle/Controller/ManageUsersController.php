446424e6af59e958ee1cbe517a6254243150981b
Simongioffredi <simon@spin-interactive.com>
2018-11-19 17:20:50 +0100

    all

--- src/AppBundle/Controller/ManageUsersController.php
+++ src/AppBundle/Controller/ManageUsersController.php
@@ -3,20 +3,20 @@
     namespace AppBundle\Controller;
 
     use AppBundle\Entity\User;
-    use AppBundle\Entity\Categorie;
     use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\Form\Extension\Core\Type\TextType;
     use Symfony\Component\Form\Extension\Core\Type\PasswordType;
     use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-    use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+    use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
     class ManageUsersController extends Controller {
-        function suscribeAction(Request $request) {
+        function suscribeAction(Request $request, UserPasswordEncoderInterface $passwordEncoder) {
             $user = new User();
 
             $form = $this->createFormBuilder($user)
-                ->add('name', TextType::class)
+                ->add('username', TextType::class)
                 ->add('password', PasswordType::class)
                 ->add('save', SubmitType::class, array('label' => 'S\'inscrire'))
                 ->getForm();
@@ -24,6 +24,10 @@
             $form->handleRequest($request);
 
             if ($form->isSubmitted() && $form->isValid()) {
+                $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+
+                $user->setPassword($password);
+
                 $entityManager = $this->getDoctrine()->getManager();
                 $entityManager->persist($user);
                 $entityManager->flush();
@@ -31,12 +35,21 @@
                 return $this->redirectToRoute('homepage');
             }
 
-            return $this->render('default/connexion.html.twig', array(
+            return $this->render('default/suscribe.html.twig', array(
                 'form' => $form->createView()
             ));
         }
 
-        function connectionAction() {
+        function connectionAction(AuthenticationUtils $authenticationUtils) {
+            // get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
 
+            // last username entered by the user
+            $lastUsername = $authenticationUtils->getLastUsername();
+
+            return $this->render('default/connexion.html.twig', array(
+                'last_username' => $lastUsername,
+                'error'         => $error
+            ));
         }
     }
